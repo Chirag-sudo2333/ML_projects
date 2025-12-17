039b076876b4b4564cef0940a8a35df651518677
Chirag-sudo2333 <chiragdahal0013@gmail.com>
2025-12-17 14:25:02 +0545

    Hand game

--- Month1/handgame.py
+++ Month1/handgame.py
@@ -14,19 +14,23 @@ rules = {
     "paper" : "rock",
     "scissors" : "paper"
 }
+lu = ('r', 'p','s')
 def game():
     while True:
         opp_chc = random.choice(b)
         user_ch = str(input("Enter your choice\n R for rock, P for paper, and S for Scissors ")).lower()
-        user_chc = shrt[user_ch]
-        print("You chose " + emoji[user_chc])
-        print("Opponent chose " +emoji[opp_chc])
-        if user_chc == opp_chc:
-            print("Draw!!")
-        elif rules[user_chc] == opp_chc:
-            print("You win!!")
+        if user_ch in lu:
+            user_chc = shrt[user_ch]
+            print("You chose " + emoji[user_chc])
+            print("Opponent chose " +emoji[opp_chc])
+            if user_chc == opp_chc:
+                print("Draw!!")
+            elif rules[user_chc] == opp_chc:
+                print("You win!!")
+            else:
+                print("You lose!!")
         else:
-            print("You lose!!")
+            print("Inavlid Input!!")
         x = str(input("Do you want to continue? (y/n) ")) 
 
         if x == "n":
