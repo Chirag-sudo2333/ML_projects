e752f02c91943dbb8ddec677edb66ba680a2a602
Chirag-sudo2333 <chiragdahal0013@gmail.com>
2025-12-17 15:48:14 +0545

    Fina RPM

--- Month1/handgame.py
+++ Month1/handgame.py
@@ -1,9 +1,12 @@
 import random
+
 b = ["scissors", "paper", "rock"]
-emoji = { "rock" : "🪨",
-         "paper" : "📄",
-         "scissors" : "✃"
-         }
+emoji = { 
+    "rock" : "🪨",
+    "paper" : "📄",
+    "scissors" : "✃"
+}
+
 shrt = {
     "r" : "rock",
     "p" : "paper",
@@ -15,27 +18,47 @@ rules = {
     "scissors" : "paper"
 }
 lu = ('r', 'p','s')
-def game():
+
+def user_choice():
     while True:
         opp_chc = random.choice(b)
-        user_ch = str(input("Enter your choice\n R for rock, P for paper, and S for Scissors ")).lower()
+        user_ch = str(input("Enter your choice\n R for rock, P for paper, and S for Scissors: ")).lower()
         if user_ch in lu:
-            user_chc = shrt[user_ch]
-            print("You chose " + emoji[user_chc])
-            print("Opponent chose " +emoji[opp_chc])
-            if user_chc == opp_chc:
-                print("Draw!!")
-            elif rules[user_chc] == opp_chc:
-                print("You win!!")
-            else:
-                print("You lose!!")
+            return user_ch, opp_chc
         else:
-            print("Inavlid Input!!")
-        x = str(input("Do you want to continue? (y/n) ")) 
+            print("Invalid Input!!")
 
-        if x == "n":
-                    break
-        elif x!= "y":
-                    print("Invalid Choice.")
-            
-game()
+def emoji_value(user_ch, opp_chc):
+    user_chc = shrt[user_ch]
+    print("You chose " + emoji[user_chc])
+    print("Opponent chose " + emoji[opp_chc])
+    return user_chc
+    
+def choose_winner(user_chc, opp_chc):
+    if user_chc == opp_chc:
+        print("Draw!!")
+    elif rules[user_chc] == opp_chc:
+        print("You win!!")
+    else:
+        print("You lose!!")
+    
+def conti_user():
+    x = str(input("Do you want to continue? (y/n): ")).lower()
+    if x == "n":
+        return False
+    elif x != "y":
+        print("Invalid Choice.")
+        return False
+    else:
+        return True
+
+def play_game():  
+    while True:
+        user_ch, opp_chc = user_choice()
+        user_chc = emoji_value(user_ch, opp_chc)
+        choose_winner(user_chc, opp_chc)
+        if not conti_user():
+            break
+
+# Main code
+play_game()
